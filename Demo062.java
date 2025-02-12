7fff57d03f32536006cb29f6be11c21a6fc7e63d
Swathithippavaram <thippavaramswathi11@gmail.com>
2025-02-12 09:31:31 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,12 @@
+public class Demo062{
+    public static void main(String[] args){
+        int[] array = {10,20,30,40};
+        System.out.println("array 0: "+array[0]);
+        System.out.println("array 1: "+array[1]);
+        System.out.println("array 2: "+array[array.length-1]);
+      
+       
+        
+    }
+    
+}
