43c3799e150e8d83ec5e15f9383d88d3dab25436
Swathithippavaram <thippavaramswathi11@gmail.com>
2025-02-12 09:34:07 +0530

    Add files via upload

---
+++ Demo065.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Demo065 {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5};
+        
+         System.out.println(Arrays.toString(array));
+         for (int element : array) {
+            System.out.println(element);
+
+
+    }
+    
+}
+}
