fc060626eba7ba6306170d700405b206814fa963
Swathithippavaram <thippavaramswathi11@gmail.com>
2025-02-14 21:32:30 +0530

    Add files via upload

---
+++ Demo069.java
@@ -0,0 +1,12 @@
+public class Demo069 {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5,6,7,8};
+        int sum =0;
+        for(int i=0;i<array.length;i++){
+            sum += array[i];
+            System.out.println("the sum of even numbers: "+sum); 
+            
+        }
+    }
+    
+}
